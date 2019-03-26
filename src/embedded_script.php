efac59cfce23ac3806743a732e52120efc805181
dolijonviter <feterjhon17@gmail.com>
2019-03-26 23:01:03 +0700

    update buku informatika PHP7

---
+++ src/embedded_script.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Contoh Embedded</title>
+</head>
+<body>
+	<?php
+	echo "Hai, Saya dari Script PHP";
+	?>
+
+</body>
+</html>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Contoh Embedded</title>
+</head>
+<body>
+Hai, Saya dari Script PHP
+</body>
+</html>
+
+<?php
+echo "<html>";
+echo "<head>";
+echo 	"<title>";
+echo "Ini merupakan Script Embedded";
+echo 	"</title>";
+echo "</head>";
+echo "<body>";
+echo "<p>ini teks yang dihasilkan dengan menggunakan dokumen script PHP </p>";
+echo "</body>";
+echo "</html>";
+?>
+
+
+<html><head><title>  Ini merupakan Script Embedded </title></head><body>
+<p>ini teks yang dihasilkan dengan menggunakan dokumen script PHP</p>
+</body></html>
