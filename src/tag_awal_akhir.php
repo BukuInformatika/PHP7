05b00aa29b5284a544d4383bdd65eb8dda31c64f
dolijonviter <feterjhon17@gmail.com>
2019-03-27 23:46:49 +0700

    update buku informatika PHP7

---
+++ src/tag_awal_akhir.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Script PHP</title>
+</head>
+<body>
+<h2>Berikut cara-cara penulisan tag-tag PHP</h2>
+</body>
+
+<!-- Cara Pertama -->
+<?php
+echo "Hai, Saya script PHP dengan cara pertama";
+?>
+
+<br>
+<!-- Cara Kedua -->
+<?
+echo "Hai, Saya script PHP dengan cara kedua";
+?>
+
+<br>
+<!-- Cara Ketiga dan keempat -->
+<%
+echo "Hai, Saya script PHP dengan cara kedua";
+$srt = "Hai, Saya script PHP dengan cara kedua";
+%>
+<% =$str; %>
+
+</html>
+
+$namauser
+$password
+$kota2
+$tempat_lahir
