be9798bceca97e58edfd1630a23cc29275abe255
dolijonviter <feterjhon17@gmail.com>
2019-04-03 22:37:09 +0700

    update buku informatika PHP7

--- src/lingkupVariabel.php
+++ src/lingkupVariabel.php
@@ -24,3 +24,40 @@ function test() {
 test();
 echo $c; 
 ?> 
+
+
+
+
+<?php
+function test() {
+    $a = 5; // Variable local
+    echo "<p>Variable a didalam function adalah: $a</p>";
+}
+test();
+ 
+// Jika kita memanggil variable local maka tidak akan ada nilainya
+echo "<p>Variable a diluar function adalah: $a</p>";
+?> 
+ 
+// Output program
+// Variable a didalam function adalah: 5
+// Variable a diluar function adalah:
+
+
+
+<?php
+function test() {
+    static $a = 8;
+    echo $a;
+    $a++;
+}
+
+test();
+test();
+test();
+?> 
+
+// Output
+// 8
+// 9
+// 10
